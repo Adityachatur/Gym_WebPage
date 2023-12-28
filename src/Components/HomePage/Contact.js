8f0e6217b7318cf34293fc3bfa34b9763d8026e2
Adityachatur <66326241+Adityachatur@users.noreply.github.com>
2023-12-28 22:31:20 +0530

    New changes

--- src/Components/HomePage/Contact.js
+++ src/Components/HomePage/Contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
   return (
     <div>
       <div className='text-white bg-black flex lg:justify-evenly justify-start lg:flex-row flex-wrap md:flex-cols py-3'>
-        <div className='flex mx-4 text-2xl card ' data-aos="fade-right">
+        <div className='flex mx-4 text-2xl card ' data-aos="fade-up">
           <FontAwesomeIcon icon={faLocationPin} className='mx-4 my-2 icon-bg bg-orange-900  text-black h-8 w-8 p-4 rounded-full' />
           <p className='mt-5 lg:text-sm text-base font-Montserrat cursor-pointer '>Balajinagar medaknrwaidi chakan, <br /> 410501</p>
         </div>
@@ -20,7 +20,7 @@ const Contact = () => {
           <FontAwesomeIcon icon={faMobileScreenButton} className='mx-4 bg-orange-900 text-black icon-bg h-8 w-8 p-4 rounded-full' />
           <p className='mt-5 lg:text-sm text-base font-Montserrat cursor-pointer'>+91 9552314201 </p>
         </div>
-        <div className='flex mx-4 my-2 text-2xl card ' data-aos="fade-left">
+        <div className='flex mx-4 my-2 text-2xl card ' data-aos="fade-up">
           <FontAwesomeIcon icon={faEnvelope} className='mx-4 bg-orange-900 h-8 w-8 p-4 icon-bg text-black rounded-full' />
           <p className='mt-5 lg:text-sm text-base font-Montserrat cursor-pointer'>adityachatur7996@gmail.com</p>
         </div>
