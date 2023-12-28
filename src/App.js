2dcb0dbdaccd4ebd326fb46373c5fc32a5830ae1
Adityachatur <66326241+Adityachatur@users.noreply.github.com>
2023-12-28 22:17:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Homepage from './Components/HomePage/Homepage';
+import { Routes, Route } from 'react-router-dom';
+import AboutPage from './Components/Aboutpage/AboutPage';
+import ServicePage from './Components/ServicePage/ServicePage';
+import ContactPage from './Components/ContactPage/ContactPage';
+import WhatsAppButton from './Components/Pages/WhatsAppButton ';
+import Gallery from './Components/Gallery/Gallery';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/About' element={<AboutPage />} />
+        <Route path='/Service' element={<ServicePage />} />
+        <Route path='/Gallery' element={<Gallery/>}/>
+        <Route path='/Contact' element={<ContactPage />} />
+      </Routes>
+      <WhatsAppButton/>
+
+
     </div>
   );
 }
